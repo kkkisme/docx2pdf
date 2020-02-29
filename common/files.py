9aae94c15eee7dcc1b7d8e06e71f25632dc4123b
huming <100huming@gmail.com>
2020-02-29 18:02:16 +0800

    fix: resolve pdf url

--- common/files.py
+++ common/files.py
@@ -1,10 +1,11 @@
 import os
 from config import config
 from werkzeug.utils import secure_filename
+from flask import request
 
 
 def uploads_url(path):
-    return path.replace(config['uploads_dir'], '/uploads')
+    return request.host_url + path.replace(config['uploads_dir'], 'uploads')
 
 
 def save_to(folder, file):
